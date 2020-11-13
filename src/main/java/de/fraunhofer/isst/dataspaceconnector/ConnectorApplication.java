7ecf9b34c433d2c7db111807a5bb18bb1d65c03d
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2020-11-13 11:20:42 +0100

    chore: load ip and port at connector start

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/ConnectorApplication.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/ConnectorApplication.java
@@ -1,14 +1,21 @@
 package de.fraunhofer.isst.dataspaceconnector;
 
+import de.fraunhofer.isst.dataspaceconnector.message.ArtifactMessageHandler;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import javax.annotation.PostConstruct;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * This is the main application class. The application is started and an openApi bean for the Swagger UI is created.
@@ -25,6 +32,7 @@ import org.springframework.context.annotation.ComponentScan;
         "de.fraunhofer.isst.ids.framework.spring.starter"
 })
 public class ConnectorApplication {
+    public static final Logger LOGGER = LoggerFactory.getLogger(ConnectorApplication.class);
     /**
      * <p>main.</p>
      *
@@ -53,5 +61,13 @@ public class ConnectorApplication {
                         )
                         .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0.txt"))
                 );
+    }
+
+    @PostConstruct
+    public void loadAccessUrl() throws UnknownHostException {
+        String address = InetAddress.getLocalHost().getHostAddress();
+        String name = InetAddress.getLocalHost().getHostName();
+
+        LOGGER.info(address + " " + name);
     }
 }
